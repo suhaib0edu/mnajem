0601a61b9bdbde37e0fd74c675cb67058adb7613
env-suhaib <suhab.edu@gmail.com>
2023-11-04 18:16:49 +0200

    اضفة الخط و بعض عناصر ال SVG  و تعريفها

---
+++ lib/app/utils/theme/svg.dart
@@ -0,0 +1,14 @@
+const String followSVG = 'assets/svg/follow.svg';
+const String verifiedSVG = 'assets/svg/verified.svg';
+
+const String likeSVG = 'assets/svg/like.svg';
+const String likeFillSVG = 'assets/svg/like_fill.svg';
+const String commentsSVG = 'assets/svg/comments.svg';
+const String viewsSVG = 'assets/svg/views.svg';
+const String moreSVG = 'assets/svg/more.svg';
+
+const String forYouSVG = 'assets/svg/for_you.svg';
+const String frindesSVG = 'assets/svg/frindes.svg';
+const String imgPostSVG = 'assets/svg/img_post.svg';
+const String textImgPostSVG = 'assets/svg/text_img_post.svg';
+const String textPostSVG = 'assets/svg/text_post.svg';
