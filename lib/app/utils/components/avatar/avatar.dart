d93ebf3ef70df58b16e8f15da801f68a070f9d89
env-suhaib <suhab.edu@gmail.com>
2023-11-13 10:44:13 +0200

    create avatar.dart

---
+++ lib/app/utils/components/avatar/avatar.dart
@@ -0,0 +1,42 @@
+import 'package:mnajem/app/exports.dart';
+
+class Avatar extends StatelessWidget {
+  final String? name;
+  final bool isVerified;
+  const Avatar({super.key, this.name, this.isVerified = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Stack(
+          children: [
+            SuhContainer(
+              height: 35,
+              width: 35,
+              border: Border.all(
+                color: baseColor.withOpacity(0.4),
+                width: 1.5,
+              ),
+            ),
+            if (isVerified)
+              Positioned(
+                bottom: 0,
+                left: 3,
+                child: SvgPicture.asset(
+                  verifiedSVG,
+                  width: 18,
+                ),
+              )
+          ],
+        ),
+        if (name != null)
+          SuhText(
+            text: name,
+            fontSize: 12,
+            color: textColor1,
+          )
+      ],
+    );
+  }
+}
