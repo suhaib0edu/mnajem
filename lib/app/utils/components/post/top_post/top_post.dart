6c4f9d0f6e583c3546dd92c84a002a956bd3c64c
env-suhaib <suhab.edu@gmail.com>
2023-11-13 10:37:30 +0200

    create top post UI

---
+++ lib/app/utils/components/post/top_post/top_post.dart
@@ -0,0 +1,21 @@
+import 'package:mnajem/app/exports.dart';
+
+class TopPost extends StatelessWidget {
+  const TopPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Avatar(
+          name: 'صهيب الطيب',
+          isVerified: true,
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        FollowButton(),
+      ],
+    );
+  }
+}
