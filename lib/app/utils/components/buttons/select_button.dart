cf7385720f5c3be4718cf816caefb279d3abc777
env-suhaib <suhab.edu@gmail.com>
2023-11-20 18:43:06 +0200

    انشاء زر الاختيار

---
+++ lib/app/utils/components/buttons/select_button.dart
@@ -0,0 +1,30 @@
+import '../../../exports.dart';
+
+class SelectButton extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  const SelectButton({super.key, required this.text, this.isSelected = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return SuhButton(
+      bkgColor: Colors.transparent,
+      borderSideColor: baseColor.withOpacity(0.5),
+      child: Row(
+        children: [
+          SuhContainer(
+            height: 20,
+            width: 20,
+            margin: EdgeInsets.only(left: 4),
+            padding: EdgeInsets.zero,
+            border: Border.all(color: textColor1, width: 2),
+            color: isSelected ? baseColor.withOpacity(0.7) : null,
+          ),
+          SuhText(
+            text: text,
+          ),
+        ],
+      ),
+    );
+  }
+}
