cfff3828f358d99cf39db05248bd006403e99909
env-suhaib <suhab.edu@gmail.com>
2023-11-24 15:52:21 +0200

    الوان خاصة بالظلال

--- lib/app/utils/theme/colors.dart
+++ lib/app/utils/theme/colors.dart
@@ -10,3 +10,5 @@ const Color textColor1 = Color.fromRGBO(185, 185, 185, 1);
 const Color textColor2 = Color.fromRGBO(216, 216, 216, 1);
 
 const Color iconC0lor = Color.fromRGBO(151, 147, 147, 1);
+const Color shadowC0lor = Color.fromRGBO(31, 31, 31, 1);
+const Color shadowC0lor1 = Color.fromRGBO(85, 85, 85, 1);
