4ad4e9e74b6179d7b272a81712c20ebc38ff79f3
env-suhaib <suhab.edu@gmail.com>
2023-11-29 15:16:10 +0200

    اضافية خاصية الضغط على صورة الملف الشخصي

--- lib/app/utils/components/avatar/avatar.dart
+++ lib/app/utils/components/avatar/avatar.dart
@@ -4,7 +4,8 @@ class Avatar extends StatelessWidget {
   final String? name;
   final bool isVerified;
   final double? size;
-  const Avatar({super.key, this.name, this.isVerified = false, this.size});
+  final void Function()? onTap;
+  const Avatar({super.key, this.name, this.isVerified = false, this.size, this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -12,26 +13,29 @@ class Avatar extends StatelessWidget {
       mainAxisAlignment:
           name == null ? MainAxisAlignment.center : MainAxisAlignment.start,
       children: [
-        Stack(
-          children: [
-            SuhContainer(
-              height: size ?? 35,
-              width: size ?? 35,
-              border: Border.all(
-                color: baseColor.withOpacity(0.4),
-                width: 1.5,
-              ),
-            ),
-            if (isVerified)
-              Positioned(
-                bottom: 0,
-                left: 3,
-                child: SvgPicture.asset(
-                  verifiedSVG,
-                  width: 18,
+        InkWell(
+          onTap: onTap,
+          child: Stack(
+            children: [
+              SuhContainer(
+                height: size ?? 35,
+                width: size ?? 35,
+                border: Border.all(
+                  color: baseColor.withOpacity(0.4),
+                  width: 1.5,
                 ),
-              )
-          ],
+              ),
+              if (isVerified)
+                Positioned(
+                  bottom: 0,
+                  left: 3,
+                  child: SvgPicture.asset(
+                    verifiedSVG,
+                    width: 18,
+                  ),
+                )
+            ],
+          ),
         ),
         if (name != null)
           SuhText(
