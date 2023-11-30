9f56255d8129eb2ccd7557e247afff3efab70ddd
env-suhaib <suhab.edu@gmail.com>
2023-11-30 11:10:24 +0200

    تعديل الاعرض و الارتفاع

--- lib/app/utils/widgets/suh_list_view_b.dart
+++ lib/app/utils/widgets/suh_list_view_b.dart
@@ -4,17 +4,20 @@ class SuhListViewB extends StatelessWidget {
   final Widget? Function(BuildContext, int) itemBuilder;
   final int? itemCount;
   final bool isHorizontal;
+  final double? height;
+  final double? width;
   const SuhListViewB({
     super.key,
     required this.itemBuilder,
     this.itemCount,
-    this.isHorizontal = false,
+    this.isHorizontal = false, this.height, this.width,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 48,
+      height: height,
+      width: width,
       child: crFun(),
     );
   }
