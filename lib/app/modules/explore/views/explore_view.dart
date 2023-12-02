759d75883ef42772908a4c20ec9934b86a61c138
env-suhaib <suhab.edu@gmail.com>
2023-12-02 09:53:28 +0200

    اضافة عناصر لصفحة الاستكشاف

--- lib/app/modules/explore/views/explore_view.dart
+++ lib/app/modules/explore/views/explore_view.dart
@@ -1,18 +1,20 @@
-
 import 'package:mnajem/app/exports.dart';
+import 'package:mnajem/app/modules/explore/views/hashtag/hashtag_view.dart';
 
 import '../controllers/explore_controller.dart';
+import 'frindes/frindes_view.dart';
 
 class ExploreView extends GetView<ExploreController> {
   const ExploreView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return SuhScaffold(
-      body: const Center(
-        child: Text(
-          'ExploreView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        children: const [
+          AppBarView(),
+          HashtagView(),
+          FrindesView(),
+        ],
       ),
     );
   }
