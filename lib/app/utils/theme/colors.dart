65c6e469259de83822534844bd9b9cb39bb1f0a1
env-suhaib <suhab.edu@gmail.com>
2023-12-02 09:51:18 +0200

    تحديث الالوان

--- lib/app/utils/theme/colors.dart
+++ lib/app/utils/theme/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color baseColor = Color.fromRGBO(51, 151, 225, 1);
 const Color backgroundC = Color.fromRGBO(30, 30, 30, 1);
+const Color backgroundC1 = Color.fromARGB(255, 20, 20, 20);
 const Color container = Color.fromRGBO(45, 45, 45, 1);
 const Color container1 = Color.fromRGBO(64, 68, 75, 1);
 const Color container2 = Color.fromRGBO(69, 69, 69, 1);
