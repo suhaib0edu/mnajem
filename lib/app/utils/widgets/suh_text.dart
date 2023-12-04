4c168b92b7217b4705eed09412b388388bbe1206
env-suhaib <suhab.edu@gmail.com>
2023-12-04 08:53:01 +0200

    تحديث تنسيق النص

--- lib/app/utils/widgets/suh_text.dart
+++ lib/app/utils/widgets/suh_text.dart
@@ -2,16 +2,24 @@ import 'package:mnajem/app/exports.dart';
 
 class SuhText extends StatelessWidget {
   final String? text;
+  final TextAlign? textAlign;
   final double? fontSize;
   final FontWeight? fontWeight;
   final Color? color;
-  const SuhText(
-      {super.key, this.text, this.fontSize, this.fontWeight, this.color});
+  const SuhText({
+    super.key,
+    this.text,
+    this.textAlign,
+    this.fontSize,
+    this.fontWeight,
+    this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text ?? '',
+      textAlign: textAlign,
       style: TextStyle(
         fontFamily: 'Cairo',
         fontSize: fontSize,
