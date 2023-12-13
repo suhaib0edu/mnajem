71b86fcdfcf094ecd8f2325b0af643661ea5c1cb
env-suhaib <suhab.edu@gmail.com>
2023-12-13 12:02:17 +0200

    رمز اضافة منشور جديد

--- lib/app/utils/theme/svg.dart
+++ lib/app/utils/theme/svg.dart
@@ -9,16 +9,17 @@ const String commentsSVG = 'assets/svg/post/comments.svg';
 const String viewsSVG = 'assets/svg/post/views.svg';
 const String moreSVG = 'assets/svg/post/more.svg';
 //post
+const String addPostSVG = 'assets/svg/post/add_post.svg';
 const String forYouSVG = 'assets/svg/post/for_you.svg';
 const String frindesSVG = 'assets/svg/post/frindes.svg';
 const String imgPostSVG = 'assets/svg/post/img_post.svg';
 const String textImgPostSVG = 'assets/svg/post/text_img_post.svg';
 const String textPostSVG = 'assets/svg/post/text_post.svg';
 //menu
-const String homeSVG = 'assets/svg/menu/home.svg'; 
-const String marketSVG = 'assets/svg/menu/market.svg'; 
-const String shortStorySVG = 'assets/svg/menu/short_story.svg'; 
-const String workSVG = 'assets/svg/menu/work.svg'; 
-const String logoutSVG = 'assets/svg/menu/logout.svg'; 
-const String searchSVG = 'assets/svg/menu/search.svg'; 
+const String homeSVG = 'assets/svg/menu/home.svg';
+const String marketSVG = 'assets/svg/menu/market.svg';
+const String shortStorySVG = 'assets/svg/menu/short_story.svg';
+const String workSVG = 'assets/svg/menu/work.svg';
+const String logoutSVG = 'assets/svg/menu/logout.svg';
+const String searchSVG = 'assets/svg/menu/search.svg';
 const String walletSVG = 'assets/svg/menu/Wallet.svg';
