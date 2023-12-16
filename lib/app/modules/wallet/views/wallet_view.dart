cd3f40c78151d581c911114b88180cbf9a281a86
env-suhaib <suhab.edu@gmail.com>
2023-12-16 10:46:34 +0200

    تحديث صفحة المحفظة

--- lib/app/modules/wallet/views/wallet_view.dart
+++ lib/app/modules/wallet/views/wallet_view.dart
@@ -1,16 +1,60 @@
 import 'package:mnajem/app/exports.dart';
-
 import '../controllers/wallet_controller.dart';
+import 'balance_view.dart';
+import 'transfers_view.dart';
 
 class WalletView extends GetView<WalletController> {
   const WalletView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return SuhScaffold(
-      body: const Center(
-        child: Text(
-          'WalletView is working',
-          style: TextStyle(fontSize: 20),
+      body: ListView(
+        children: [
+          const SuhContainer(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SuhText(
+                  text: 'رقم حسابك',
+                ),
+                SuhText(
+                  text: '2398567',
+                  fontSize: 20,
+                ),
+                Avatar(),
+              ],
+            ),
+          ),
+          BalanceView(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              bankButton(
+                'شحن المحفظة',
+                const Color.fromARGB(255, 27, 94, 32),
+              ),
+              bankButton(
+                'تحويل خارحي',
+                Color.fromARGB(255, 158, 73, 27),
+              ),
+            ],
+          ),
+          bankButton(
+            'تحويل لحساب آخر',
+            const Color.fromARGB(255, 23, 76, 119),
+          ),
+          TransfersView(),
+        ],
+      ),
+    );
+  }
+
+  bankButton(String? text, Color? color) {
+    return ContainerButton(
+      color: color,
+      child: Center(
+        child: SuhText(
+          text: text,
         ),
       ),
     );
