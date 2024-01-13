8290d56a5450ebf4de12ea97bb674ebca700c472
env-suhaib <suhab.edu@gmail.com>
2024-01-13 23:18:48 +0200

    اضافة بعض الخصائص

--- lib/app/utils/widgets/suh_text_field.dart
+++ lib/app/utils/widgets/suh_text_field.dart
@@ -2,18 +2,31 @@ import '../../exports.dart';
 
 class SuhTextField extends StatelessWidget {
   final String? hintText;
-  const SuhTextField({super.key, this.hintText});
+  final int? minLines;
+  final int maxLines;
+  final TextEditingController? controller;
+  final void Function(String)? onChanged;
+  const SuhTextField({
+    super.key,
+    this.hintText,
+    this.minLines,
+    this.maxLines = 1, this.controller, this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SuhContainer(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      border: Border.all(color: baseColor),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      border: Border.all(color: baseColor.withOpacity(0.2)),
       child: TextField(
-        style: TextStyle(color: textColor2, fontSize: 12),
+        controller: controller,
+        onChanged: onChanged,
+        minLines: minLines,
+        maxLines: maxLines,
+        style: const TextStyle(color: textColor2, fontSize: 12),
         decoration: InputDecoration(
           hintText: hintText,
-          hintStyle: TextStyle(
+          hintStyle: const TextStyle(
             color: textColor,
             fontSize: 12,
           ),
